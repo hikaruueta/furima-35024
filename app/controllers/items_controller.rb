064740714ae9933a788714bbe3b7818af93a77bc
hikaruueta <hkr.u.e.t.0821@gmail.com>
2021-04-26 17:49:26 +0900

    商品一覧#1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
-  end   
+    @item = Items.all.order("created_at DESC")
+  end  
+  
+  def edit
+  end  
+
+  
 end
