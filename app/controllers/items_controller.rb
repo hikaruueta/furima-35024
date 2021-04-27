71e9acbfed65f19cf0f9f60957ada960c45d6889
hikaruueta <hkr.u.e.t.0821@gmail.com>
2021-04-27 12:32:33 +0900

    修正#1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,14 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:edit, :update, :show]
+  before_action :redirect, only: [:edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
   end  
 
   def edit
-    redirect_to root_path if current_user.id != @item.user_id
   end
 
   def update
@@ -47,5 +47,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def redirect
+    redirect_to root_path if current_user.id != @item.user_id
   end
 end
