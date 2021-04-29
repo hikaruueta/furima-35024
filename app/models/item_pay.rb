35d7404843dd6a34c659802b02912fb0eab684ba
hikaruueta <hkr.u.e.t.0821@gmail.com>
2021-04-29 17:00:37 +0900

    修正

--- app/models/item_pay.rb
+++ app/models/item_pay.rb
@@ -5,7 +5,7 @@ class ItemPay
   with_options presence: true do
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :address, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid.  Input half-width character & not hyphen(-)' }
 
     validates :token
